11aa43f1392a750e82cfee7bd9616b9c5ee40305
Max Wenzin <max.wenzin@gmail.com>
2015-06-24 20:05:38 +0200

    Trying out the acuator to get /health etc

--- src/main/java/se/bettercode/shorturl/ShortUrlApplication.java
+++ src/main/java/se/bettercode/shorturl/ShortUrlApplication.java
@@ -25,13 +25,16 @@ public class ShortUrlApplication implements CommandLineRunner {
 
 
     public void run(String... args) throws Exception {
+        /*
         for (ShortUrl u : repository.findAll()) {
             log.debug(u.toString());
         }
+        */
         log.debug("Short URLs in DB: " + repository.count());
     }
     
     public static void main(String[] args) {
         SpringApplication.run(ShortUrlApplication.class, args);
     }
+
 }
--- src/main/java/se/bettercode/shorturl/WelcomeController.java
+++ src/main/java/se/bettercode/shorturl/WelcomeController.java
@@ -2,8 +2,6 @@ package se.bettercode.shorturl;
 
 import java.util.Date;
 import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
